27b786a8acaf98aef74aa9a9951ce3795a418ed7
touseefx2 <x2app123@gmail.com>
2025-11-03 22:57:38 +0500

    Update index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 // import * as Localization from "expo-localization";
-import { Storage } from "@/src/services/storage";
+import { SecureStorageService } from "@/src/services/storage";
 import { I18nManager } from "react-native";
 
 import en from "@/src/locales/en/translation.json";
@@ -15,14 +15,14 @@ const resources = {
 };
 
 export const initI18n = async () => {
-  const savedLang = await Storage.getItem("language");
+  const savedLang = await SecureStorageService.getItem("language");
   // Localization.getLocales()[0]?.languageCode || "en";
   const lng = savedLang ||  "en";
 
   i18n
     .use(initReactI18next)
     .init({
-      compatibilityJSON: "v3",
+      compatibilityJSON: "v4",
       resources,
       lng,
       fallbackLng: "en",
